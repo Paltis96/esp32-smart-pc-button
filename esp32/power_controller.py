16c2d39357a03066677e9a3871c8e775938f1e60
Denys Kovshun <deniskovshun@gmail.com>
2026-03-17 16:50:04 +0200

    fix: removed unused vars and improved controller logic

--- esp32/power_controller.py
+++ esp32/power_controller.py
@@ -17,33 +17,13 @@ class PowerController:
         self._pin = machine.Pin(pin, machine.Pin.OUT)
 
         self.host_history = []
-        self._host_status = False
         self.target_history = []
-        self._targer_status = False
         self._pulse_s = pulse_s
         self._next_allowed_trigger_time = 0
         self._history_limit = history_limit
-    @property
-    def host_status(self):
-        return self._state
+   
 
-    @host_status.setter
-    def host_status(self, value):
-        if not isinstance(value, bool):
-            raise ValueError(f"Invalid state '{value}'.")
-        self._host_status = value
-
-    @property
-    def targer_status(self):
-        return self._state
-
-    @targer_status.setter
-    def targer_status(self, value):
-        if not isinstance(value, bool):
-            raise ValueError(f"Invalid state '{value}'.")
-        self.targer_status = value
-
-    async def _tcp_ping(self, host, port=80, timeout=2):
+    async def _tcp_check(self, host, port=80, timeout=2):
         try:
             logger.debug(f"Ping: {host}")
             conn = asyncio.open_connection(host, port)
@@ -60,20 +40,14 @@ class PowerController:
         if len(history) > self._history_limit:
             history.pop(0)
 
-    def _is_all_true(self, history) -> bool:
-        if self._config.status_sample_size >= history:
-            history_sample = history
-        else:
-            slice_list = self._config.status_sample_size * -1
-            history_sample = history[slice_list:]
-            
-        if len(history_sample) < self._config.status_sample_size:
+    def _stable_state(self, history):
+        if len(history) < self._config.status_sample_size:
             return False
-        return all(history_sample)
+        return all(history[-self._config.status_sample_size:])
 
-    def trigger_switch(self):
+    async def trigger_switch(self):
         self._pin.on()
-        sleep(self._pulse_s)
+        await asyncio.sleep(self._pulse_s)
         self._pin.off()
         logger.info("Power signal sent")
 
@@ -92,26 +66,28 @@ class PowerController:
             logger.warning('Target IP address not set.')
             return
 
-        host_online = await self._tcp_ping(str(self._config.host_ip))
+        host_online = await self._tcp_check(str(self._config.host_ip))
         await asyncio.sleep(0.01)
-        target_online = await self._tcp_ping(str(self._config.target_ip))
+        target_online = await self._tcp_check(str(self._config.target_ip))
         
         hs = 'up' if host_online else 'down'
         ts = 'up' if target_online else 'down'
-
         
-        logger.info(f"Host: {hs} | Target: {ts}")
-            
+        logger.info(f"Host {self._config.host_ip}: {hs} | Target {self._config.target_ip}: {ts}")
+        
         self._push_state(self.host_history, host_online)
         self._push_state(self.target_history, target_online)
-
-        if not host_online and self._is_all_true(self.target_history):
+        
+        host_stable = self._stable_state(self.host_history)
+        target_stable = self._stable_state(self.target_history)
+        
+        if not host_stable and target_stable:
             if now >= self._next_allowed_trigger_time:
                 logger.info('Triggering a host to wake up...')
-                self.trigger_switch()
+                await self.trigger_switch()
                 self.set_delay(self._config.retry_delay_s)
-                logger.info(f"Next try in {self._next_allowed_trigger_time} | delay = {self._config.retry_delay_s}")
-
+                logger.info(f"Next retry in {self._config.retry_delay_s}s")
+                
     def status(self):
         data = {"host_history": self.host_history,
                 "target_history": self.target_history,
